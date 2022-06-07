70bc5df06dc5f479eacb8bbd1caaede013a6474e
Your NameThejas <thejas69gg@gmail.com>
2022-06-07 18:59:02 -0300

    The filters are useful

--- main.py
+++ main.py
@@ -2,7 +2,11 @@
 from ast import Break, Pass, main
 from fileinput import close
 import os
+from this import d
+from numpy import datetime_data
 import pandas as pd
+from datetime import date, time, datetime
+
 
 def wachStock():
     #archiveStock = "Stock.txt"
@@ -21,81 +25,97 @@ def addProduct():
     print("Se añadirá un producto")
     #archiveStock = "Stock.txt"
     #archive = open(archiveStock, "w")
-    newProduct = {"code":"","product":"","stock":"","cost":"","benefit":"","income":""}
-    newProduct["code"] = input("Escribe el código del nuevo producto")
-    newProduct["product"] = str(input("Escribe el nombre del producto"))
-    newProduct["stock"] = int(input("Escribe la cantidad del producto"))
-    newProduct["cost"] = float(input("Escribe cual fue el costo del producto"))
-    newProduct["benefit"] = float(input("Escribe cual es el margen de ganancias del producto"))
-    newProduct["income"] = float(input("Escribe cual es el precio del producto"))
+    newProduct = {"code":"","product":"","stock":"","cost":"","benefit":"","income":"","date":""}
+    newProduct["code"] = input("Escribe el código del nuevo producto: ")
+    newProduct["product"] = str(input("Escribe el nombre del producto: "))
+    newProduct["inventary"] = int(input("Escribe la cantidad del producto: "))
+    newProduct["cost"] = float(input("Escribe cual fue el costo del producto: "))
+    newProduct["benefit"] = float(input("Escribe cual es el margen de ganancias del producto: "))
+    newProduct["price"] = float(input("Escribe cual es el precio del producto: "))
+    caducatedDate = str(input("Escribe la fecha de caducidad: "))
+    dateProduct = datetime.strptime(caducatedDate, "%d/%m/%y")
+    newProduct["date"] = dateProduct
     archive = archive.append(newProduct, ignore_index=True)
     print(archive)
+    archive.to_excel("base de datos de stock.xlsx")
 
-#def cantFilter():
-    #print(numberFilter)
-    #archiveStock = "Base de datos de stock.xlsx"
-    #archive = pd.read_excel(archiveStock)
-    #filterCant = float(input("Escriba el tipo de cantidad que desea filtrar"))
-    #if filterCant == 1:
-       #cantProduct = int(input("Escribe la cantidad del producto"))
-        #archive = archive[archive["cant"]==int(cantProduct)]
-        #print(archive)
-    #elif filterCant == 2:
-        #cantProduct = int(input("Escribe la cantidad del producto"))
-        #archive = archive[archive["cant"]>=int(cantProduct)]
-        #print(archive)
-    #elif filterCant == 3:
-        #cantProduct = int(input("Escribe la cantidad del producto"))
-        #archive = archive[archive["cant"]<=int(cantProduct)]
-        #print(archive)
-    #else:
-        #print("No eligio ninguna de las opciones posibles")
-
-#def tipeFilter():
-    #archiveStock = "Base de datos de stock.xlsx"
-    #archive = pd.read_excel(archiveStock)
-    #fil = int(input("Escriba el identificador del filtro que desea aplicar"))
-    #if fil == 1:
-        #introduceCode = int(input("Escribe el código de un producto"))
-        #archive = archive[archive["code"]==int(introduceCode)]
-        #print(archive)
-    #eliif fil == 2:
-        #nameProduct = str(input("Escribe el nombre de un producto"))
-        #archive = archive[archive["product"]==str(nameProduct)]
-        #print(archive)
-    #eliif fil == 3:
-        #cantFilter()
-    #eliif fil == 4:
-        #costProduct = float(input("Escribe el costo de un producto"))
-        #archive = archive[archive["cost"]==float(costProduct)]
-        #print(archive)
-    #eliif fil == 5:
-        #benefitProduct = float(input("Escribe el beneficio del producto"))
-        #archive = archive[archive["benefit"]==float(benefitProduct)]
-        #print(archive)
-    #eliif fil == 6:
-        #priceProduct = float(input("Escribe el beneficio del producto"))
-        #archive = archive[archive["price"]==float(benefitProduct)]
-        #print(archive)
-    #eliif fil == 7:
-        #line = int(input("Escriba el número de la fila que desea filtrar"))
-        #archive = archive.iloc[int(line)]
-        #print(archive)
-    #elif fil == 8:
-        #column = str(input("Escribe el nombre de la columna que deseas filtrar"))
-        #archive = archive[column]
-    #elif fil == 9:
-        #Pass
-    #else:
-        #Print("Eliga una opción de las disponibles")
-
-def filter():
-    print("Se mostrará el producto de acuerdo a su código")
+def cantFilter():
     archiveStock = "Base de datos de stock.xlsx"
     archive = pd.read_excel(archiveStock)
-    introduceCode = int(input("Escribe el código de un producto"))
-    archive = archive[archive["code"]==int(introduceCode)]
-    print(archive)
+    while True:
+        print(numberFilter)
+        filterCant = int(input("Seleccione como desea filtrar la cantidad: "))
+        if filterCant == 1:
+            cantProduct = int(input("Escribe la cantidad del producto: "))
+            archive = archive[archive["inventary"]==int(cantProduct)]
+            print(archive)
+        elif filterCant == 2:
+            cantProduct = int(input("Escribe la cantidad minima del producto: "))
+            archive = archive[archive["inventary"]>=int(cantProduct)]
+            print(archive)
+        elif filterCant == 3:
+            cantProduct = int(input("Escribe la cantidad máxima del producto: "))
+            archive = archive[archive["inventary"]<=int(cantProduct)]
+            print(archive)
+        elif filterCant == 4:
+            return False
+        else:
+            print("No eligio ninguna de las opciones posibles")
+
+def filter():
+    archiveStock = "Base de datos de stock.xlsx"
+    archive = pd.read_excel(archiveStock)
+    while True:
+        print(filterOptions)
+        fil = int(input("Escriba el identificador del filtro que desea aplicar: "))
+        if fil == 1:
+            introduceCode = int(input("Escribe el código de un producto: "))
+            archive = archive[archive["code"]==int(introduceCode)]
+            print(archive)
+        elif fil == 2:
+            nameProduct = str(input("Escribe el nombre de un producto: "))
+            archive = archive[archive["product"]==str(nameProduct)]
+            print(archive)
+        elif fil == 3:
+            cantFilter()
+        elif fil == 4:
+            costProduct = float(input("Escribe el costo de un producto: "))
+            archive = archive[archive["cost"]==float(costProduct)]
+            print(archive)
+        elif fil == 5:
+            benefitProduct = float(input("Escribe el beneficio del producto: "))
+            archive = archive[archive["benefit"]==float(benefitProduct)]
+            print(archive)
+        elif fil == 6:
+            priceProduct = float(input("Escribe el beneficio del producto: "))
+            archive = archive[archive["price"]==float(priceProduct)]
+            print(archive)
+        elif fil == 7:
+            line = int(input("Escriba el número de la fila que desea filtrar: "))
+            archive = archive.iloc[int(line)]
+            print(archive)
+        elif fil == 8:
+            column = str(input("Escribe el nombre de la columna que deseas filtrar: "))
+            archive = archive[column]
+            print(archive)
+        elif fil == 9:
+            caducatedDate = str(input("Escribe la fecha de caducidad: "))
+            dateProduct = datetime.strptime(caducatedDate, "%d/%m/%y")
+            print(dateProduct)
+            archive = archive[archive["date"]==(dateProduct)]
+            print(archive)
+        elif fil == 10:
+            return False
+        else:
+            print("Eliga una opción de las disponibles")
+
+#def filter():
+    #print("Se mostrará el producto de acuerdo a su código")
+    #archiveStock = "Base de datos de stock.xlsx"
+    #archive = pd.read_excel(archiveStock)
+    #introduceCode = int(input("Escribe el código de un producto: "))
+    #archive = archive[archive["code"]==int(introduceCode)]
+    #print(archive)
     #acá se muestra las opciones de filtro y se selecciona la que irá en tipeFilter
     #print(filterOptions)
     #tipeFilter()
@@ -123,6 +143,7 @@ def menu ():
 
         elif posibility == 3:
             print("Desea modificar algún producto")
+            modifi()
             
         elif posibility == 4:
             print("Desea eliminar algún producto")
@@ -135,7 +156,7 @@ def menu ():
             
         elif posibility == 7:
             print("Mostrar productos por vencer")
-            
+
         elif posibility == 8:
             print("Cargar las ventas diarias")
             
@@ -177,11 +198,13 @@ filterOptions="""
 6)_ Para filtrar de acuerdo al precio del producto
 7)_ Para filtrar por filas
 8)_ Para filtrar por columnas
-9)_ Terminar el filtrado
+9)_ Para filtrar por fechas de caducidad
+10)_ Para dejar de filtrar
 """
 numberFilter="""
 1)_ Una cantidad exacta
 2)_ Una cantidad minima
-2)_ Una cantidad máxima
+3)_ Una cantidad máxima
+4)_ Dejar de filtrar por cantidad
 """
 posibility = menu()
--- try.py
+++ try.py
@@ -1,3 +1,5 @@
+import datetime
+from datetime import date, time, datetime
 import pandas as pd
 
 archiveStock = "Base de datos de stock.xlsx"
@@ -29,10 +31,14 @@ print(archive)
 #archive = archive[archive["product"]==str(introduceCode)]
 
 #Filtrar por producto
-#introduceCode = int(input("Escribe el código de un producto"))
-#archive = archive[archive["code"]==int(introduceCode)]
+#introduceCode = str(input("Escribe el código de un producto"))
+#archive = archive[archive["product"]==str(introduceCode)]
 #line = int(input("Escriba el número de la fila que desea filtrar"))
 #archive = archive.iloc[int(line)]
-#column = str(input("Escribe el nombre de la columna que deseas filtrar"))
-#archive = archive[column]
+column = str(input("Escribe el nombre de la columna que deseas filtrar"))
+archive = archive[column]
+#def calculatorBenefit ():
+    #moneyBenefit = ("price" - "cost") * "cant sells"
+    #print(moneyBenefit)
+
 print(archive)
